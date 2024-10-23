b6e6b9963723d480fe10bfc56d7ea6bb11d7fa88
Seb0927 <sebastianidrobo@hotmail.com>
2024-10-23 12:03:13 -0500

    fix: target url was not working

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL,
+        target: 'https://dev.same.com.co/api/public/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
