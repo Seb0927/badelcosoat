01ffdd2f646efc3756a1bd19720ba9605a5040ec
Seb0927 <sebastianidrobo@hotmail.com>
2024-10-23 20:00:42 -0500

    fix: "cancelar" button from modal not working

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -45,7 +45,7 @@ function Modal({setisModalVisible, data, formData}) {
           {/* Modal footer */}
           <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b ">
             <button data-modal-hide="medium-modal" type="button" className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Continuar</button>
-            <button data-modal-hide="medium-modal" type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-orange-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Cancelar</button>
+            <button data-modal-hide="medium-modal" type="button" onClick={() => setisModalVisible(false)} className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-orange-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Cancelar</button>
           </div>
         </div>
       </div>
